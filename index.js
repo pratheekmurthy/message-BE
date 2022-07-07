72678568c5f79006bdc5da3c498461c12186c2b1
Pratheek <pratheekmurthy>
2022-07-07 13:50:49 +0530

    db connection & route creation

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')  
+const configureDB = require('./database/db')
+const router = require('./routes/messageRoutes')
 const app = express()
-
 const Port = 4000
+
+
+configureDB()
 app.use(express.json())
+app.use(router)
 
 
 app.listen(Port,()=>{
